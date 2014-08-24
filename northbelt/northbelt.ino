841a2664bb63906a333f6364ef3a34a2eef1043f
David Singleton <davidsingleton@gmail.com>
2014-08-24 12:50:03 -0700

    Use correct axis for orientation in belt. Factor out DEBUG code. Add fancy calibration UI.

--- northbelt/northbelt.ino
+++ northbelt/northbelt.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>
 #include <LSM303.h>
 
+#define DEBUG 1
+
 LSM303 compass;
 LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32768, -32768, -32768};
 
-int CALIBRATE_LOOPS = 100;
+int CALIBRATE_LOOPS = 500;
 int VIBE_BASE = 4;
 int VIBE_COUNT = 16;
 int SMOOTHING_DEGREES = 5;
+int LED_PIN = 13;
 
 float CENTER_WEDGE = (360 / VIBE_COUNT) / 2;
 
@@ -17,7 +20,31 @@ int vibrateCycles = 0;
 float heading = 0.0;
 float lastVibratedHeading = 180.0;
 
+void makeCalibrateStartVibePattern() {
+  for (int i = VIBE_BASE; i < VIBE_BASE + VIBE_COUNT; i++) {
+    digitalWrite(i, HIGH);
+    delay(250);
+    digitalWrite(i, LOW);
+  }
+}
+
+void makeCalibrateDoneVibePattern() {
+  for (int j = 0; j < 2; j++) {
+    for (int i = VIBE_BASE; i < VIBE_BASE + VIBE_COUNT; i++) {
+      digitalWrite(i, HIGH);
+    }
+    delay(250);
+    for (int i = VIBE_BASE; i < VIBE_BASE + VIBE_COUNT; i++) {
+      digitalWrite(i, LOW);
+    }
+  }
+}
+
 void calibrate() {
+
+  makeCalibrateStartVibePattern();
+  
+  int led = HIGH;
   
   for(int i = 0; i < CALIBRATE_LOOPS; i++) {
     compass.read();
@@ -30,12 +57,40 @@ void calibrate() {
     running_max.y = max(running_max.y, compass.m.y);
     running_max.z = max(running_max.z, compass.m.z);
     
-    delay(100);
+    digitalWrite(LED_PIN, led);
+    if (led == HIGH) {
+      led = LOW;
+    } else {
+      led = HIGH;
     }
+    
+    delay(50);
+  }
+
+  if (DEBUG) {
+    Serial.print("calibrate ");
+    Serial.print(running_min.x);
+    Serial.print(" ");
+    Serial.print(running_min.y);
+    Serial.print(" ");
+    Serial.print(running_min.z);
+    Serial.print(", ");
+    Serial.print(running_max.x);
+    Serial.print(" ");
+    Serial.print(running_max.y);
+    Serial.print(" ");
+    Serial.print(running_max.z);
+    Serial.println();
+  }
+  
+  makeCalibrateDoneVibePattern();
 }
 
 void setup() {
   Serial.begin(9600);
+  if (DEBUG) {
+    Serial.println("DEBUG");
+  }
   Wire.begin();
   compass.init();
   compass.enableDefault();
@@ -43,6 +98,8 @@ void setup() {
   for (int i = 0; i < VIBE_COUNT; i++) {
     pinMode(VIBE_BASE + i, OUTPUT);
   }
+  
+  pinMode(LED_PIN, OUTPUT);
   
   calibrate();
 
@@ -56,14 +113,17 @@ void maybeVibrateHeading() {
   if (abs(lastVibratedHeading - heading) > SMOOTHING_DEGREES) {
     lastVibratedHeading = heading;
 
-    Serial.println("****");    
     wedge = (((int)(heading + CENTER_WEDGE)) % 360);
     wedge = wedge / (360 / VIBE_COUNT);
-    Serial.println(wedge);
-    
     motor = wedge + VIBE_BASE;
-    Serial.println(motor);
-    Serial.println(" ** ");
+
+    if (DEBUG) {
+      Serial.print("vibe ");
+      Serial.print(wedge);
+      Serial.print(" ");
+      Serial.print(motor);
+      Serial.println();
+    }
     digitalWrite(currentVibePin, LOW);
     digitalWrite(motor, HIGH);
     currentVibePin = motor;
@@ -84,27 +144,18 @@ void loop() {
   compass.read();
   
   /*
-  When given no arguments, the heading() function returns the angular
-  difference in the horizontal plane between a default vector and
-  north, in degrees.
-  
-  The default vector is chosen by the library to point along the
-  surface of the PCB, in the direction of the top of the text on the
-  silkscreen. This is the +X axis on the Pololu LSM303D carrier and
-  the -Y axis on the Pololu LSM303DLHC, LSM303DLM, and LSM303DLH
-  carriers.
-  
-  To use a different vector as a reference, use the version of heading()
-  that takes a vector argument; for example, use
-  
-    compass.heading((LSM303::vector<int>){0, 0, 1});
-  
-  to use the +Z axis as a reference.
+   compass.heading((LSM303::vector<int>){0, 0, 1});
+     => use the +Z axis as a reference (points directly through the PCB from
+     silkscreened side to populated side).
   */
-  heading = compass.heading();
+  heading = compass.heading((LSM303::vector<int>){0, 0, 1});
   maybeVibrateHeading();
   vibrateClock();
   
-  Serial.println(heading);
+  if (DEBUG) {
+    Serial.print("heading ");
+    Serial.println(heading);
+  }
+
   delay(100);
 }
